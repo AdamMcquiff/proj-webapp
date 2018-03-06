f97964b93c5c52c6754256c1401359248bd15fb3
Adam McKenna <adam-mckenna@outlook.com>
2018-03-06 16:37:53 +0000

    Resolved bug that stopped API data being requested on initial login

--- src/app/common/services/http.service.ts
+++ src/app/common/services/http.service.ts
@@ -1,25 +1,31 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { environment } from "../../../environments/environment";
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    "Content-Type": "application/json; charset=utf-8",
-    "Authorization": "Bearer " + localStorage.getItem('token')
-  })
-};
+import { environment } from "../../../environments/environment";
 
 @Injectable()
 export class HttpService {
   baseUrl = environment.api_url;
+  httpOptions = {};
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.refreshToken();
+  }
 
   get(endpoint) {
-    return this.http.get(this.baseUrl + endpoint, httpOptions);
+    return this.http.get(this.baseUrl + endpoint, this.httpOptions);
   }
 
   post(endpoint, data) {
-    return this.http.post(this.baseUrl + endpoint, data, httpOptions);
+    return this.http.post(this.baseUrl + endpoint, data, this.httpOptions);
+  }
+
+  refreshToken() {
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json; charset=utf-8",
+        "Authorization": "Bearer " + localStorage.getItem('token')
+      })
+    };
   }
 }
