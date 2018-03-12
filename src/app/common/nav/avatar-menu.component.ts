2dfbed9d6042c62ec0845f3b40c4115376364a10
Adam McKenna <adam-mckenna@outlook.com>
2018-03-12 15:28:17 +0000

    Basic layout for avatar-menu built

--- src/app/common/nav/avatar-menu.component.ts
+++ src/app/common/nav/avatar-menu.component.ts
@@ -1,6 +1,11 @@
 import { Component, Input } from "@angular/core";
 import { Router } from "@angular/router";
 
+import { AuthService } from "../services/auth.service";
+import { HttpService } from "../services/http.service";
+
+import { APIResponse } from "../interfaces/api-response.interface";
+
 @Component({
   selector: "avatar-menu",
   templateUrl: "./avatar-menu.component.html",
@@ -9,6 +14,17 @@ import { Router } from "@angular/router";
 export class AvatarMenuComponent {
   @Input() isOpen;
 
+  user: Object;
+
+  constructor(public auth: AuthService, private httpService: HttpService) {}
+
+  ngOnInit(): void {
+    this.httpService.get('profile')
+      .subscribe((data: APIResponse) => {
+        this.user = data.data;
+      });  
+  }
+  
   toggleDialog() {
     this.isOpen = !this.isOpen;
   }
