159a79b4f0735012a920cea839129e3e41d36954
Adam McKenna <adam-mckenna@outlook.com>
2018-03-12 14:08:22 +0000

    Added dynamic organisation title to header

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -2,6 +2,9 @@ import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 
 import { AuthService } from "../services/auth.service";
+import { HttpService } from "../services/http.service";
+
+import { APIResponse } from "../interfaces/api-response.interface";
 
 @Component({
   selector: "primary-header",
@@ -9,9 +12,16 @@ import { AuthService } from "../services/auth.service";
 })
 
 export class HeaderComponent {
-  title = "header";
+  user: Object;
 
-  constructor(public auth: AuthService) {}
+  constructor(public auth: AuthService, private httpService: HttpService) {}
+
+  ngOnInit(): void {
+    this.httpService.get('profile')
+      .subscribe((data: APIResponse) => {
+        this.user = data.data;
+      });  
+  }
 
   public signout() {
     this.auth.signout();
