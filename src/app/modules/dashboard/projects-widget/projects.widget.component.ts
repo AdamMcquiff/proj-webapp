2202f7038b4d235c98ab8c4fa13fb279affe93e3
Adam McKenna <adam-mckenna@outlook.com>
2018-03-19 14:18:09 +0000

    Linting

--- src/app/modules/dashboard/projects-widget/projects.widget.component.ts
+++ src/app/modules/dashboard/projects-widget/projects.widget.component.ts
@@ -1,23 +1,24 @@
 import { Component, Input } from "@angular/core";
 
+import { Project } from "../../projects/project.model";
+
 import { APIResponse } from "../../../common/interfaces/api-response.interface";
 import { HttpService } from "../../../common/services/http.service";
 
 @Component({
   selector: "dashboard-projects-widget",
-  templateUrl: "./projects.widget.component.html",
-  providers: []
+  templateUrl: "./projects.widget.component.html"
 })
 
 export class ProjectsDashboardWidgetComponent {
-  projects: Object = null
+  projects: Array<Project>;
 
   constructor(private httpService: HttpService) {}
 
   ngOnInit(): void {
     this.httpService.get('projects')
       .subscribe((data: APIResponse) => {
-        this.projects = data.data;
+        this.projects = <Array<Project>>data.data;
       });  
   }
 }
--- src/app/modules/dashboard/tasks-widget/tasks.widget.component.ts
+++ src/app/modules/dashboard/tasks-widget/tasks.widget.component.ts
@@ -1,24 +1,23 @@
 import { Component, Input } from "@angular/core";
 
+import { Task } from "../../projects/tasks/task.model";
+
 import { APIResponse } from "../../../common/interfaces/api-response.interface";
 import { HttpService } from "../../../common/services/http.service";
 
 @Component({
   selector: "dashboard-tasks-widget",
-  templateUrl: "./tasks.widget.component.html",
-  providers: []
+  templateUrl: "./tasks.widget.component.html"
 })
 
 export class TasksDashboardWidgetComponent {
-  tasks: Object = null
+  tasks: Array<Task>;
 
   constructor(private httpService: HttpService) {}
 
   ngOnInit(): void {
     this.httpService.get('tasks')
-      .subscribe((data: APIResponse) => {
-        this.tasks = data.data;
-      });  
+      .subscribe((data: APIResponse) => this.tasks = <Array<Task>>data.data);  
   }
 }
   
